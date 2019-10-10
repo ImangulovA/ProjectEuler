fe1344e4b63e3ba5c73e0f8ead11520306f004a7
Imangulov <imangulovamal@gmail.com>
2019-10-10 18:12:46 +0300

    Create 18.py

---
+++ 18.py
@@ -0,0 +1,16 @@
+# Find the maximum total from top to bottom of the triangle given in input.
+
+
+
+for _ in range(int(input())):
+    n = int(input())
+    s = []
+    for i in range(n):
+        s.append(list(map(int, input().split())))
+    
+    row = n-2
+    while row >= 0:
+        for i in range(len(s[row])):
+            s[row][i] += max(s[row+1][i], s[row+1][i+1])
+        row -= 1
+    print(s[0][0])
